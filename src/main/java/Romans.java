d7d1f1e3fea1d113b30453e4ff1b31115857e2a8
olavhl <56083131+olavhl@users.noreply.github.com>
2020-09-07 08:44:31 +0200

    Added some functionality to do 50

--- src/main/java/Romans.java
+++ src/main/java/Romans.java
@@ -2,29 +2,22 @@ public class Romans {
     public String toRomans(int i) {
         String result = "";
 
-        if ( i < 9 ){
+        if ( i < 10 ){
             result = addNumberTens(i);
-        }
-
-         else if (i == 9) {
-            result = "IX";
-        } else if (i == 10) {
-            result = "X";
-         } else if ( i < 19) {
+        } else if ( i < 20) {
              result = "X";
              result += addNumberTens(i);
-        } else if ( i == 19) {
-             result = "XIX";
-        } else if (i == 20) {
-             result = "XX";
-        } else if ( i < 29) {
+        }  else if ( i < 30) {
              result = "XX";
              result += addNumberTens(i);
-        } else if ( i == 29) {
-             result = "XXIX";
-        } else if (i < 39) {
+        } else if (i < 40) {
              result = "XXX";
              result += addNumberTens(i);
+        } else if (i < 50) {
+             result = "XL";
+             result += addNumberTens(i);
+        } else if (i == 50) {
+             result = "L";
         }
 
         return result;
@@ -38,9 +31,11 @@ public class Romans {
             result = addRomanOnes(remainder, 0);
         } else if (remainder == 4) {
             result = "IV";
-        } else if (remainder > 4) {
+        } else if ( remainder < 9){
             result = "V";
             result += addRomanOnes(remainder, 5);
+        } else {
+            result = "IX";
         }
 
         return result;
@@ -55,4 +50,5 @@ public class Romans {
 
         return str;
     }
+
 }
